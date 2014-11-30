c815afcb98a06d34ef06ad873fd0751d58ae1460
Iwan-Zotow <iwanzotow@gmail.com>
2014-11-30 14:03:27 -0500

    Seeding clean-up, default seeds are primes now

--- xorshift1024star.hpp
+++ xorshift1024star.hpp
@@ -14,47 +14,58 @@ namespace OTI
         public: typedef result_type   seed_type;
 
         // those values are from S.Vigna "An experimental exploration of Marsaglia's xorshift generators, scrambled",
-        // http://arxiv.org/abs/1402.6246, known as M(12,25,27) with scrambling
-        public: static constexpr uint32_t shift_a = 12U;
-        public: static constexpr uint32_t shift_b = 25U;
-        public: static constexpr uint32_t shift_c = 27U;
+        // http://arxiv.org/abs/1402.6246, known as M(31,11,30) with scrambling
+        public: static constexpr uint32_t shift_a = 31U;
+        public: static constexpr uint32_t shift_b = 11U;
+        public: static constexpr uint32_t shift_c = 30U;
 
-        public: static constexpr result_type  mult  = 2685821657736338717ULL;
+        public: static constexpr result_type  mult  = 1181783497276652981ULL;
 
-        public: static constexpr seed_type    default_seed   = 1ULL;        
+        public: static constexpr seed_type    default_seed[] = { 3001ULL, 3079ULL, 3257ULL, 3511ULL,
+                                                                 4001ULL, 4057ULL, 4507ULL, 4583ULL,
+                                                                 5003ULL, 5281ULL, 5861ULL, 5987ULL,
+                                                                 6007ULL, 6229ULL, 6301ULL, 7901ULL };
 
         public: static constexpr float        norm  = float{double{1.0}/double{result_type{-1LL}}};
 #pragma endregion
 
 #pragma region Data
         private: mutable seed_type _seed[16];
+        private: mutable int       _p;
 #pragma endregion
 
 #pragma region Ctor/Dtor/op=
-        public: xorshift1024star(const seed_type* seed):
-            _seed{seed[0], seed[1], seed[2], seed[3], seed[4]}
+        public: xorshift1024star(const seed_type* seed = default_seed):
+            _seed{seed[0], seed[1], seed[2], seed[3], seed[4], seed[5], seed[6], seed[7], seed[8], seed[9], seed[10], seed[11], seed[12], seed[13], seed[14], seed[15]},
+            _p{0}
         {
         }
 
         public: xorshift1024star(xorshift1024star const& r):
-            _seed{r._seed}
+            xorshift1024star{r._seed},
+            _p{r._p}
         {
         }
 
         public: xorshift1024star(xorshift1024star&& r):
-            _seed{std::move(r._seed)}
+            xorshift1024star{r._seed},
+            _p{r._p}
         {
         }        
 
         public: xorshift1024star& operator=(xorshift1024star const& r)
         {
-            _seed = r._seed;
+            std::copy(r._seed, r._seed + 16, _seed);
+            _p = r._p;
+
             return *this;
         }
 
-        public: xorshift64star& operator=(xorshift1024star&& r)
+        public: xorshift1024star& operator=(xorshift1024star&& r)
         {
-            _seed = std::move(r._seed);
+            std::copy(r._seed, r._seed + 16, _seed);
+            _p = r._p;
+
             return *this;
         }
 
@@ -64,33 +75,31 @@ namespace OTI
 #pragma endregion
 
 #pragma region Observers
-        public: seed_type seed() const
+        public: const seed_type* seed() const
         {
             return _seed;
         }
 #pragma endregion
 
 #pragma region Helpers
-        public: static seed_type sample(seed_type seed)
-        {
-            seed ^= seed >> shift_a;
-            seed ^= seed << shift_b;
-            seed ^= seed >> shift_c;
-
-            return mult * seed;
-        }
 #pragma endregion
 
 #pragma region Mutators
-        public: void sample() const
+        public: result_type sample() const
         {
-            _seed = sample(_seed);
+            uint64_t s0 = seed[ _p ];
+            _p = ( _p + 1 ) & 15;
+            uint64_t s1 = _seed[ _p ];
+
+            s1 ^= s1 << shift_a;
+            s1 ^= s1 >> shift_b;
+            s0 ^= s0 >> shift_c;
+            return ( _seed[ _p ] = s0 ^ s1 ) * mult;
         }
 
         public: float number() const
         {
-            sample();
-            return float(_seed) * norm;
+            return float(sample()) * norm;
         }
 
         public: void skip(uint64_t ns) const;
